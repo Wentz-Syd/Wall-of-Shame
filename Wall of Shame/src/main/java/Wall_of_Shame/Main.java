cfbdfa9a9e03dfbcb196be40b81dc93e4f4dd690
Syd Wentz <wentz.syd@gmail.com>
2024-12-15 10:01:54 -0500

    initial push

---
+++ Wall of Shame/src/main/java/Wall_of_Shame/Main.java
@@ -0,0 +1,7 @@
+package Wall_of_Shame;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Wall of Shame/src/main/java/Wall_of_Shame/model/Game.java
@@ -0,0 +1,12 @@
+package Wall_of_Shame.model;
+
+public class Game {
+    private String name;
+    private String type;
+    private String publisher;
+    private String description;
+    private int releaseYear;
+    private String platform;
+    private boolean completed;
+
+}
